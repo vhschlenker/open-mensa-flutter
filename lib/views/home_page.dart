a146f630bf98f1db5d7bf3e19b97879624dfb9e2
Veit-Hendrik Schlenker <git@vhschlenker.de>
2018-09-23 10:53:30 +0200

    Add tabbar to switch between canteens

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -7,6 +7,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String _selectedDate = _dateChoices[0];
+  List<String> _canteens = _generateCanteens(10);
 
   void _selectDate(String dateChoice) {
     setState(() {
@@ -16,27 +17,45 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: new Text('OpenMensa'), actions: <Widget>[
-      new PopupMenuButton(
-        child: new Padding(
-          padding: new EdgeInsets.only(right: 24.0),
-          child: new Center(
-              child: new Text(
-            _selectedDate,
-            style: new TextStyle(fontWeight: FontWeight.bold),
-          )),
-        ),
-        onSelected: _selectDate,
-        itemBuilder: (BuildContext context) {
-          return _dateChoices.where((choice) {
-            return choice != _selectedDate;
-          }).map((String dateChoice) {
-            return PopupMenuItem(value: dateChoice, child: Text(dateChoice));
-          }).toList();
-        },
-      )
-    ]));
+    return DefaultTabController(
+        length: _canteens.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: new Text('OpenMensa'),
+            actions: <Widget>[
+              new PopupMenuButton(
+                child: new Padding(
+                  padding: new EdgeInsets.only(right: 24.0),
+                  child: new Center(
+                      child: new Text(
+                    _selectedDate,
+                    style: new TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                ),
+                onSelected: _selectDate,
+                itemBuilder: (BuildContext context) {
+                  return _dateChoices.where((choice) {
+                    return choice != _selectedDate;
+                  }).map((String dateChoice) {
+                    return PopupMenuItem(
+                        value: dateChoice, child: Text(dateChoice));
+                  }).toList();
+                },
+              )
+            ],
+            bottom: TabBar(
+              tabs: _canteens.map((canteen) {
+                return Tab(text: canteen);
+              }).toList(),
+              isScrollable: true,
+            ),
+          ),
+          body: TabBarView(
+            children: _canteens.map((canteen) {
+              return Center(child: Text(canteen));
+            }).toList(),
+          ),
+        ));
   }
 }
 
@@ -45,3 +64,11 @@ const List<String> _dateChoices = const <String>[
   'Morgen (24.09)',
   'Übermorgen (25.09)'
 ];
+
+List<String> _generateCanteens(int count) {
+  var canteensList = List<String>(count);
+  for (var i = 0; i < count; i++) {
+    canteensList[i] = 'Mensa $i';
+  }
+  return canteensList;
+}
